6969476edc050c4ce319781e9b520aaa27e576e6
Michael Eselgroth <meselgroth@yahoo.com>
2020-06-15 16:35:45 +1000

    ChangeColour Loop through RGBA bytes and apply bitwise operations

--- canvas.mjs
+++ canvas.mjs
@@ -3,13 +3,18 @@ export default class Canvas {
         this.canvasHtmlElement = canvasHtmlElement;
         this.ctx = this.canvasHtmlElement.getContext('2d');
     }
-    async Add(imgElement){
-        let imageBitmap = await createImageBitmap(imgElement);
-        this.ctx.drawImage(imageBitmap,0,0);
+    async Add(imgElement) {
+        this.imageBitmap = await createImageBitmap(imgElement);
+        this.ctx.drawImage(this.imageBitmap, 0, 0);
     }
     ChangeColour() {
         let ctx = this.ctx;
-        ctx.fillStyle = 'green';
-        ctx.fillRect(10, 10, 150, 100);
+
+        let imageData = ctx.getImageData(0, 0, 100, 100); // Get RGBA bytes
+        for (let i = 0; i < imageData.data.length; i++) {
+            if (i !== 0 && i % 4 === 0) continue; // skip alpha values
+            imageData.data[i] = imageData.data[i]>>1; // left shift RGB values by 1 bit
+        }
+        ctx.putImageData(imageData, 0, 0);
     }
 }
